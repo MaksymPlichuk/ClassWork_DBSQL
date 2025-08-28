7e6a402034b3dc42f267ba56e4386d0955a8ded2
Maksym <pilipm449@gmail.com>
2025-08-28 21:37:45 +0300

    Tasks 5-12 done HW

--- 04_RelationsForeignKeys/04_Hospital.sql
+++ 04_RelationsForeignKeys/04_Hospital.sql
@@ -52,6 +52,26 @@ create table Wards(
 	DepartmentId int not null references Departments(ID)
 );
 
+----
+alter table Doctors
+add DepartmentID int not null references Departments(ID) default(1)
+
+create table Examinations(
+	ID int identity not null primary key,
+	DayOfWeek int not null check(DayOfWeek between 1 and 7),
+	EndTime time not null,
+	Name nvarchar(100) not null check(Name <>'') unique,
+	StartTime time not null check(StartTime between '8:00' and '18:00'),
+	DoctorID int not null references Doctors(ID),
+	check(EndTime > StartTime)
+);
+create table Diseases(
+	ID int identity not null primary key,
+	Name nvarchar(100) not null check(Name <>'') unique,
+	Severity int not null check(Severity !<1) default(1),
+	SpecializationID int not null references Specializations(ID)
+);
+----
 select * from Wards
 select * from Departments
 select * from Donations
@@ -77,7 +97,41 @@ select distinct dp.Name
 from Departments as dp,Sponsors as s, Donations as d
 Where(s.Name like 'Umbrella Corporation' and d.SponsorID = s.ID and dp.ID = d.DepartmentId)
 
---5
+select dp.Name,s.Name,d.Ammount,d.Date
+from Departments as dp, Donations as d, Sponsors as s
+Where(dp.ID=d.DepartmentId and s.ID=d.SponsorID)
+
+--6 невистачає таблиці
+select d.Surname,dp.Name
+from Doctors as d, Examinations as e, Departments as dp
+Where(e.DoctorID=d.ID and d.DepartmentID = dp.ID and e.DayOfWeek between 1 and 5)
+
+--7 невистачає таблиці
+select w.Name,dp.Name
+from Departments as dp, Wards as w, Doctors as d
+Where(d.Name like 'Helen' and d.Surname like 'Williams' and d.DepartmentID = dp.ID and w.DepartmentId=dp.ID)
+
+select dp.Name,doc.Name+' ' +doc.Surname as [FullName]
+from Departments as dp,Donations as d, Doctors as doc
+Where(d.DepartmentId = dp.ID and d.Ammount>100000 and doc.DepartmentID = dp.ID)
+
+select dp.Name
+from Departments as dp, Doctors as doc
+Where(dp.ID=doc.DepartmentID and doc.Premium<0)
+
+--10 невистачає таблиці
+select sp.Name
+from Specializations as sp,Diseases as ds
+Where(ds.SpecializationID=sp.ID and ds.Severity>3)
+
+--11 невистачає таблиці
+select dp.Name,ds.Name
+from Diseases as ds, Examinations as e, Departments as dp
+
+--12 невистачає таблиці
+select dp.Name,w.Name
+from Departments as dp, Wards as w, Diseases as d
+
 
 -----------
 
@@ -104,3 +158,9 @@ values(1, 1),(2, 2),(3, 3),(4, 4),(5, 5),(6, 6),(7, 7),(8, 8),(9, 9),(10, 10);
 
 insert into Vacations(EndDate,StartDate,DoctorId)
 values('2025-02-15','2025-02-01',1),('2025-03-10','2025-02-25',2),('2025-04-05','2025-03-20',3),('2025-05-12','2025-04-30',4),('2025-06-25','2025-06-10',5),('2025-07-15','2025-07-01',6),('2025-08-10','2025-07-28',7),('2025-09-18','2025-09-01',8),('2025-10-30','2025-10-15',9),('2025-11-20','2025-11-05',10);
+
+insert into Examinations(DayOfWeek,EndTime,Name,StartTime, DoctorID)
+values(5,'18:00','Lecheniye','10:00',1),(6,'18:00','KOVID','09:00',2),(1,'19:00','TRAVA','09:00',3),(3,'14:00','Ebola Terapy','12:00',4)
+
+insert into Diseases(Name,Severity,SpecializationID)
+values('Grip',5,4),('Rak',4,6),('Spid',2,3)
